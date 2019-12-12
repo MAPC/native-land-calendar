2352bda9e74381ff206dc2fc6c6e5810739f886c
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-12-12 16:09:02 -0500

    Finalize geojson for borders

--- _posts/2019-12-12-native-lands.md
+++ _posts/2019-12-12-native-lands.md
@@ -4,6 +4,6 @@ title:  "Native Lands"
 date:   2019-12-12 08:49:40 -0500
 ---
 
-<svg class="youth-asthma-map map" viewBox="0 0 900 700" style="border: 1px solid red;"></svg>
+<svg class="youth-asthma-map map" viewBox="0 0 700 500" style="border: 1px solid #292929;"></svg>
 <script src="/assets/javascripts/d3.min.js"> </script>
 <script src="/assets/javascripts/native-lands.js"></script>
--- assets/javascripts/native-lands.js
+++ assets/javascripts/native-lands.js
@@ -1,8 +1,8 @@
 const mapProjection = d3.geoAlbers()
-.scale(43500)
-  .rotate([71.057, 0])
-  .center([.1,42.4])
-  .translate([960 / 2, 500 / 2])
+.scale(36500)
+.rotate([71.057, 0])
+.center([.4,42.37])
+.translate([960 / 2, 500 / 2])
 
 function asthmaHtmlValue(data) {
 
@@ -44,43 +44,6 @@ function tooltipTop(event, tooltip){
   }
 }
 
-
-function asthmaCreateHeatmap(data) {
-  const tooltip = d3.select("body").append("div")
-                   .attr("class", "tooltip")
-                   .style("opacity", 0);
-                  
-  const colors = d3.scaleSequentialQuantile([50.6,495])
-      .interpolator(d3.interpolateRdPu)
-      .domain(data.features.map(feature => (feature.properties.right_yasth_arte)));
-  const youthAsthma = d3.select('.youth-asthma-map');
-  const path = d3.geoPath().projection(mapProjection);
- youthAsthma.append('g')
-    .attr('class', 'heatmap')
-    .selectAll('path')
-    .data(data.features)
-    .enter()
-    .append('path')
-    .attr('fill', d => colors(d.properties.right_yasth_arte))
-    .attr('stroke', 'red')
-    .attr('stroke-width', '1')
-    .attr('stroke-opacity', 0.6)
-    .attr('d', path)
-    // .on("mousemove", (d) => {
-    //   tooltip.transition()
-    //   .duration(50)
-    //   .style("opacity", .9);
-    //   tooltip.html(asthmaHtmlValue(d))
-    //   .style("left", tooltipLeft(d3.event, document.getElementsByClassName('tooltip')[1]))
-    //   .style("top", tooltipTop(d3.event,  document.getElementsByClassName('tooltip')[1]))
-    //   }) 
-    // .on("mouseleave", d => {
-    //   tooltip.transition()
-    //   .duration(200)
-    //   .style("opacity",0)
-    // })
-}
-
 function createBasemap(data) {
   const tooltip = d3.select("body").append("div")
                    .attr("class", "tooltip")
@@ -93,8 +56,8 @@ function createBasemap(data) {
       .data(data.features)
       .enter()
       .append('path')
-      .attr('fill', 'red')
-      .attr('stroke', 'blue')
+      .attr('fill', '#9e9ba8')
+      .attr('stroke', 'black')
       .attr('stroke-width', '1')
       .attr('stroke-opacity', 0.6)
       .attr('d', path)
@@ -115,12 +78,15 @@ function createBasemap(data) {
 
 Promise.all([
   d3.json('/assets/data/ma-basemap.geojson'),
-  d3.json('/assets/data/mapc-no-borders.geojson'),
+  d3.json('/assets/data/mapc-with-borders.geojson'),
   d3.json('/assets/data/language-intersection.geojson'),
   d3.json('/assets/data/territory-intersection.geojson'),
+  d3.json('/assets/data/mapc-no-borders.geojson')
 ]).then(data => {
 
   createBasemap(data[0])
+  // createBasemap(data[1])
+  // console.log(data[1])
 
   const baseMap = d3.select('.youth-asthma-map');
   const path = d3.geoPath().projection(mapProjection);
