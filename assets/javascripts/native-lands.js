ffd9069bf83aca91892c53328541202946d53b8d
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-12-13 14:17:14 -0500

    Display and color nations + tooltip

--- assets/javascripts/native-lands.js
+++ assets/javascripts/native-lands.js
@@ -4,28 +4,8 @@ const mapProjection = d3.geoAlbers()
 .center([.4,42.37])
 .translate([960 / 2, 500 / 2])
 
-function asthmaHtmlValue(data) {
-
-    if (data.properties.right_yasth_arte) {
-      return '<p class="info-window__section"><span class="info-window__category">Municipality</span><br />' 
-      + data.properties.municipal
-      + '</p> <p class="info-window__section"><span class="info-window__category">Age-adjusted rate per 100,000</span><br />'
-      + data.properties.right_yasth_arte
-      + '</p> <p class="info-window__section"><span class="info-window__category">Year</span><br />'
-      + data.properties.right_cal_years
-      + '</p>'
-    }
-    else {
-      if (data.properties.town) {
-        return '<p class="info-window__section"><span class="info-window__category">Municipality (no data)</span><br />' 
-        + data.properties.town
-        + '</p>' 
-      } else {
-        return '<p class="info-window__section"><span class="info-window__category">Municipality (no data)</span><br />' 
-        + data.properties.municipal
-        + '</p>' 
-      }
-  }
+function tooltipValue(data) {
+  return `<p>${data.properties.Nations}</p>`
 }
 
 function tooltipLeft(event, tooltip){
@@ -36,7 +16,7 @@ function tooltipLeft(event, tooltip){
   }
 }
 
-function tooltipTop(event, tooltip){
+function tooltipTop(event, tooltip) {
   if (event.pageY > 260) {
     return event.pageY - tooltip.offsetHeight - 10 + "px"
   } else {
@@ -44,85 +24,140 @@ function tooltipTop(event, tooltip){
   }
 }
 
-function createBasemap(data) {
+function createBasemap(state, regionWithBorders, regionWithoutBorders) {
   const tooltip = d3.select("body").append("div")
-                   .attr("class", "tooltip")
-                   .style("opacity", 0);
-  const baseMap = d3.select('.youth-asthma-map');
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+  const baseMap = d3.select('.native-land-map');
   const path = d3.geoPath().projection(mapProjection);
   baseMap.append('g')
-      .attr('class', 'heatmap')
-      .selectAll('path')
-      .data(data.features)
-      .enter()
-      .append('path')
-      .attr('fill', '#9e9ba8')
-      .attr('stroke', 'black')
-      .attr('stroke-width', '1')
-      .attr('stroke-opacity', 0.6)
-      .attr('d', path)
-      // .on("mousemove", (d) => {
-      //   tooltip.transition()
-      //   .duration(50)
-      //   .style("opacity", .9);
-      //   tooltip.html(asthmaHtmlValue(d))
-      //   .style("left", tooltipLeft(d3.event, document.getElementsByClassName('tooltip')[0]))
-      //   .style("top", tooltipTop(d3.event, document.getElementsByClassName('tooltip')[0]))
-      //   }) 
-      // .on("mouseleave", d => {
-      //   tooltip.transition()
-      //   .duration(200)
-      //   .style("opacity",0)
-      // })
+  .attr('class', 'map__basemap')
+  .selectAll('path')
+  .data(state.features)
+  .enter()
+  .append('path')
+  .attr('fill', '#9e9ba8')
+  .attr('stroke', 'black')
+  .attr('stroke-width', '1')
+  .attr('stroke-opacity', 0.6)
+  .attr('d', path)
+
+  baseMap.append('g')
+  .attr('class', 'map__region')
+  .selectAll('path')
+  .data(regionWithBorders.features)
+  .enter()
+  .append('path')
+  .attr('fill', '#9e9ba8')
+  .attr('stroke', 'black')
+  .attr('stroke-width', '1')
+  .attr('stroke-opacity', 0.6)
+  .attr('d', path)
+
+  baseMap.append('g')
+  .attr('class', 'map__region--borderless')
+  .selectAll('path')
+  .data(regionWithoutBorders.features)
+  .enter()
+  .append('path')
+  .attr('fill', '#9e9ba8')
+  .attr('stroke', 'black')
+  .attr('stroke-width', '1')
+  .attr('stroke-opacity', 0.6)
+  .attr('d', path)
+  .style('display', 'none')
+}
+
+function languageOverlay(languageData) {
+  const tooltip = d3.select(".tooltip")
+  .style("opacity", 0);
+
+  const baseMap = d3.select('.native-land-map');
+  const path = d3.geoPath().projection(mapProjection);
+  baseMap.append('g')
+  .attr('class', 'map__basemap')
+  .selectAll('path')
+  .data(languageData.features)
+  .enter()
+  .append('path')
+  .attr('fill', 'rgba(239,55,39,.4)')
+  .attr('stroke', 'black')
+  .attr('stroke-width', '1')
+  .attr('stroke-opacity', 0.6)
+  .attr('d', path)
+  .on("mousemove", (d) => {
+    tooltip.transition()
+    .duration(50)
+    .style("opacity", .9);
+    tooltip.html(tooltipValue(d))
+    .style("left", tooltipLeft(d3.event, document.getElementsByClassName('tooltip')[1]))
+    .style("top", tooltipTop(d3.event,  document.getElementsByClassName('tooltip')[1]))
+    }) 
+  .on("mouseleave", d => {
+    tooltip.transition()
+    .duration(200)
+    .style("opacity",0)
+  })
+}
+
+function territoryOverlay(territoryBoundaries, territoryData) {
+  const tooltip = d3.select(".tooltip")
+  .style("opacity", 0);
+
+  const colors = d3.scaleOrdinal(['red','blue','green','purple','yellow','cyan'])
+
+  const baseMap = d3.select('.native-land-map');
+  const path = d3.geoPath().projection(mapProjection);
+  
+  baseMap.append('g')
+  .attr('class', 'map__region')
+  .selectAll('path')
+  .data(territoryBoundaries.features)
+  .enter()
+  .append('path')
+  .attr("fill", (d,i) => colors(i))
+  .attr("fill-opacity", .4)
+  .attr('stroke', 'black')
+  .attr('stroke-width', '1')
+  .attr('stroke-opacity', 0.6)
+  .attr('d', path)
+
+  baseMap.append('g')
+  .attr('class', 'map__region')
+  .selectAll('path')
+  .data(territoryData.features)
+  .enter()
+  .append('path')
+  .attr("fill", 'white')
+  .attr("fill-opacity", 0)
+  .attr('stroke', 'black')
+  .attr('stroke-width', '1')
+  .attr('stroke-opacity', 0.6)
+  .attr('d', path)
+  .on("mousemove", (d) => {
+    tooltip.transition()
+    .duration(50)
+    .style("opacity", .9);
+    tooltip.html(tooltipValue(d))
+    .style("left", tooltipLeft(d3.event, document.getElementsByClassName('tooltip')[1]))
+    .style("top", tooltipTop(d3.event,  document.getElementsByClassName('tooltip')[1]))
+    }) 
+  .on("mouseleave", d => {
+    tooltip.transition()
+    .duration(200)
+    .style("opacity",0)
+  })
 }
 
 Promise.all([
   d3.json('/assets/data/ma-basemap.geojson'),
   d3.json('/assets/data/mapc-with-borders.geojson'),
+  d3.json('/assets/data/mapc-no-borders.geojson'),
   d3.json('/assets/data/language-intersection.geojson'),
-  d3.json('/assets/data/territory-intersection.geojson'),
-  d3.json('/assets/data/mapc-no-borders.geojson')
+  d3.json('/assets/data/full-territory.geojson'),
+  d3.json('/assets/data/overlapping-territory-data.geojson'),
 ]).then(data => {
-
-  createBasemap(data[0])
-  // createBasemap(data[1])
-  // console.log(data[1])
-
-  const baseMap = d3.select('.youth-asthma-map');
-  const path = d3.geoPath().projection(mapProjection);
-  baseMap.append('g')
-      .attr('class', 'heatmap')
-      .selectAll('path')
-      .data(data[1].features)
-      .enter()
-      .append('path')
-      .attr('fill', 'white')
-      .attr('stroke', 'black')
-      .attr('stroke-width', '1')
-      .attr('stroke-opacity', 0.6)
-      .attr('d', path)
-
-      baseMap.append('g')
-      .attr('class', 'heatmap')
-      .selectAll('path')
-      .data(data[2].features)
-      .enter()
-      .append('path')
-      .attr('fill', 'rgba(25,25,25,.3)')
-      .attr('stroke', 'green')
-      .attr('stroke-width', '1')
-      .attr('stroke-opacity', 0.6)
-      .attr('d', path)
-
-      baseMap.append('g')
-      .attr('class', 'heatmap')
-      .selectAll('path')
-      .data(data[3].features)
-      .enter()
-      .append('path')
-      .attr('fill', 'rgba(125,25,25,.4)')
-      .attr('stroke', 'green')
-      .attr('stroke-width', '1')
-      .attr('stroke-opacity', 0.6)
-      .attr('d', path)
+  createBasemap(data[0], data[1], data[2])
+  territoryOverlay(data[4], data[5])
 })
